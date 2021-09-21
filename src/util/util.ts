c7a1c0c23763e3cebab19b3996f4ed4eb57b39fa
Nsiah Akuoko Jeremiah <nakjemmy6@gmail.com>
2021-09-21 17:23:04 +0000

    project completion

---
+++ src/util/util.ts
@@ -0,0 +1,48 @@
+import fs from 'fs';
+import Jimp = require('jimp');
+
+export const TMP_DIR = __dirname + "/tmp/";
+
+// filterImageFromURL
+// helper function to download, filter, and save the filtered image locally
+// returns the absolute path to the local image
+// INPUTS
+//    inputURL: string - a publicly accessible url to an image file
+// RETURNS
+//    an absolute path to a filtered image locally saved file
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+    return new Promise(async resolve => {
+        const photo = await Jimp.read(inputURL);
+        const outpath = TMP_DIR + '/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
+        await photo
+            .resize(256, 256) // resize
+            .quality(60) // set JPEG quality
+            .greyscale() // set greyscale
+            .write(outpath, (img) => {
+                resolve(outpath);
+            });
+    });
+}
+
+// deleteLocalFiles
+// helper function to delete files on the local disk
+// useful to cleanup after tasks
+// INPUTS
+//    files: Array<string> an array of absolute paths to files
+export async function deleteLocalFiles(files: Array<string>) {
+    for (let file of files) {
+        fs.unlinkSync(file);
+    }
+}
+
+// getTmpFiles
+// a helper function to get files in tmp directory
+export async function getTmpFiles() {
+    return fs.readdirSync(TMP_DIR).map(fileName => TMP_DIR + fileName);
+}
+
+export function validateImageUrl(url: string) {
+    return url && (url.match(/\.(jpeg|jpg|gif|png)$/) != null)
+}
+
+
